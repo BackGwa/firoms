4020fe692bdfa44b7d42ef067fe95984000f3a1c
BackGwa <kojk3254@naver.com>
2022-10-09 01:50:08 +0900

    Update zasok.py

--- ext/zasok.py
+++ ext/zasok.py
@@ -1,23 +1,24 @@
 # [확장] : 자소크 철학단
 
-import sat_datetime as SatTime
-import datetime
+from sat_datetime import SatDatetime
+from datetime import datetime
 
 
 # [명령어] : 자소크력 가져오기
-def to_asn(year, month, day, hour, minute):
+def to_asn(year, month, day, hour, minute, second):
     
-    EarthNow = datetime.now()
-    ZasokNow = datetime(
-            year if year else ZasokNow.year,
-            month if month else ZasokNow.month,
-            day if day else ZasokNow.day,
-            hour if hour else ZasokNow.hour,
-            minute if minute else ZasokNow.minute
+    now = datetime.now()
+    now = datetime(
+            year if year else now.year,
+            month if month else now.month,
+            day if day else now.day,
+            hour if hour else now.hour,
+            minute if minute else now.minute,
+            second if second else now.second
         )
-    ZasokNow = SatTime.get_from_datetime(ZasokNow)
+    ZasokNow = SatDatetime.get_from_datetime(now)
     
-    result = (f'> {EarthNow.year}년 {EarthNow.month}월 {EarthNow.day}일 {EarthNow.hour}시 {EarthNow.minute}분\n'
-              f'> {ZasokNow.year}년 {ZasokNow.month}월 {ZasokNow.day}일 {ZasokNow.hour}시 {ZasokNow.minute}분')
+    result = (f'>   [UTC]   {now.year}년 {now.month:02d}월 {now.day:02d}일 {now.hour:02d}시 {now.minute:02d}분 {now.minute:02d}초\n'
+              f'>   [ASN]   {ZasokNow.year}년 {ZasokNow.month:02d}월 {ZasokNow.day:02d}일 {ZasokNow.hour:02d}시 {ZasokNow.minute:02d}분 {int(ZasokNow.second):02d}초')
     
     return result
