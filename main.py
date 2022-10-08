7c6a140374a42b29232044f64cacf6385d6eeb1f
BackGwa <kojk3254@naver.com>
2022-10-08 15:07:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import secret
+import discord
+from discord.commands import Option
+
+bot = discord.Bot()
+token = secret.token(True)
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(status=discord.Status.online)
+    
+@bot.slash_command(description='TestCommand')
+async def test(ctx):
+    await ctx.respond('TestMessage')
+
+bot.run(token)
