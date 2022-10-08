4a0bbb9973ee47673b00dd435889ea385e3741c8
BackGwa <kojk3254@naver.com>
2022-10-08 15:13:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ from discord.commands import Option
 
 bot = discord.Bot()
 token = secret.token(True)
-
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.online)
     
-@bot.slash_command(description='TestCommand')
+@bot.slash_command(guild_ids = secret.server_id(True), description='TestCommand')
 async def test(ctx):
     await ctx.respond('TestMessage')
 
