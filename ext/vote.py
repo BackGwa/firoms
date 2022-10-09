3343048fa1903df9a6ab36187c137a161f312137
BackGwa <kojk3254@naver.com>
2022-10-09 21:30:38 +0900

    Update vote.py

--- ext/vote.py
+++ ext/vote.py
@@ -17,3 +17,49 @@ def v_scan(vote, third, fourth):
         result = ('valid')
         
     return result
+
+
+# 에러 메세지
+def errmsg(index):
+    
+    if(index == 1):
+        result = ('**투표가 반영되지 않았습니다!**\n중복투표를 하거나 여러 번 투표하실 수 없습니다!')
+    else:
+        result = ('**투표가 완료되었습니다!**')
+    
+    return result
+
+
+# 프로그래스바
+def value(count):
+    
+    result = ''
+    loop = 0
+    
+    while(count > loop):
+        result += '■'
+        loop += 1
+    
+    return result
+
+
+# 메세지 새로고침
+def msg_refrash(title, content, vote, count, a, b, c, d):
+
+    valueA = value(count[0])
+    valueB = value(count[1])
+    valueC = value(count[2])
+    valueD = value(count[3])
+
+    if(vote == 2):                   
+        result = (f'**{title}**\n{content}\n{a} : {valueA}\n{b} : {valueB}')
+        
+    elif(vote == 3):
+        result = (f'**{title}**\n{content}\n{a} : {valueA}\n{b} : {valueB}\n{c} : {valueC}')
+        
+    elif(vote == 4):
+        result = (f'**{title}**\n{content}\n{a} : {valueA}\n{b} : {valueB}\n{c} : {valueC}\n{d} : {valueD}')
+        
+    print(result)
+        
+    return result
