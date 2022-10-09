d290d4bcdd323dbbb0f24a4bbc050340ff911ab6
BackGwa <kojk3254@naver.com>
2022-10-10 01:46:22 +0900

    [자소크력 > 서력] 변환기 추가

--- ext/zasok.py
+++ ext/zasok.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 # [명령어] : 자소크력 가져오기
-def to_asn(year, month, day, hour, minute, second):
+def zacalen(year, month, day, hour, minute, second):
     
     now = datetime.now()
     now = datetime(
@@ -18,7 +18,19 @@ def to_asn(year, month, day, hour, minute, second):
         )
     ZasokNow = SatDatetime.get_from_datetime(now)
     
-    result = (f'>   [UTC]   {now.year}년 {now.month:02d}월 {now.day:02d}일 {now.hour:02d}시 {now.minute:02d}분 {now.minute:02d}초\n'
-              f'>   [ASN]   {ZasokNow.year}년 {ZasokNow.month:02d}월 {ZasokNow.day:02d}일 {ZasokNow.hour:02d}시 {ZasokNow.minute:02d}분 {int(ZasokNow.second):02d}초')
+    result = (f'>   **[UTC]**   __{now.year}년 {now.month:02d}월 {now.day:02d}일 {now.hour:02d}시 {now.minute:02d}분 {now.minute:02d}초__\n'
+              f'>   **[ASN]**   __{ZasokNow.year}년 {ZasokNow.month:02d}월 {ZasokNow.day:02d}일 {ZasokNow.hour:02d}시 {ZasokNow.minute:02d}분 {int(ZasokNow.second):02d}초__')
+    
+    return result
+
+
+# [명령어] : 서력으로 자소크력 가져오기
+def inzacalen(year, month, day):
+    
+    ZasokNow = SatDatetime(year, month, day)
+    utc = ZasokNow.to_datetime()
+    
+    result = (f'>   **[ASN]**   __{year}년 {month:02d}월 {day:02d}일__\n'
+              f'>   **[UTC]**   __{utc.year}년 {utc.month:02d}월 {utc.day:02d}일__')
     
     return result
