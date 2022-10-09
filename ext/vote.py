3a47fd57430cd6dd59727b8389df9cec32567abf
BackGwa <kojk3254@naver.com>
2022-10-09 22:45:46 +0900

    투표 추가 기능 수정

--- ext/vote.py
+++ ext/vote.py
@@ -1,24 +1,6 @@
 # [확장] : 투표 및 여론조사
 
 
-# 투표 유효성 확인
-def v_scan(vote, third, fourth):
-    if((vote < 2) or (vote > 4)):
-        result = ('**선택지의 범위를 초과하였습니다!**\n투표의 선택지는 2~4개로 제한됩니다!') 
-        
-    elif((vote > 3) and (third == '')) or ((vote > 4) and ((third == '') or (fourth == ''))):
-        result = ('**선택지의 이름이 설정되지 않았습니다!**\n투표의 선택지가 2개 이상이라면 선택지의 이름을 입력해야합니다!')
-        
-        
-    elif((vote < 4) and (fourth != '')) or ((vote < 3) and (third != '')):
-        result = ('**선택지의 이름이 초과되었거나 잘못된 접근입니다!**\n이러한 명령어는 일부 내용이 손실될 수 있습니다.')
-        
-    else:
-        result = ('valid')
-        
-    return result
-
-
 # 에러 메세지
 def errmsg(index):
     
@@ -29,8 +11,7 @@ def errmsg(index):
     
     return result
 
-
-# 프로그래스바
+# 투표 프로그래스바
 def value(count):
     
     result = ''
@@ -42,24 +23,21 @@ def value(count):
     
     return result
 
-
-# 메세지 새로고침
+# 투표 새로고침
 def msg_refrash(title, content, vote, count, a, b, c, d):
-
+    
     valueA = value(count[0])
     valueB = value(count[1])
     valueC = value(count[2])
     valueD = value(count[3])
 
     if(vote == 2):                   
-        result = (f'**{title}**\n{content}\n{a} : {valueA}\n{b} : {valueB}')
+        result = (f'**{title}**\n{content}\n{a} : {valueA} ({count[0]})\n{b} : {valueB} ({count[1]})')
         
     elif(vote == 3):
-        result = (f'**{title}**\n{content}\n{a} : {valueA}\n{b} : {valueB}\n{c} : {valueC}')
+        result = (f'**{title}**\n{content}\n{a} : {valueA} ({count[0]})\n{b} : {valueB} ({count[1]})\n{c} : {valueC} ({count[2]})')
         
     elif(vote == 4):
-        result = (f'**{title}**\n{content}\n{a} : {valueA}\n{b} : {valueB}\n{c} : {valueC}\n{d} : {valueD}')
-        
-    print(result)
+        result = (f'**{title}**\n{content}\n{a} : {valueA} ({count[0]})\n{b} : {valueB} ({count[1]})\n{c} : {valueC} ({count[2]})\n{d} : {valueD} ({count[3]})')
         
     return result
